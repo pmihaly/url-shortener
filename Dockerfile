3d302c2b35ae4e6748e099df878229cc4b60925d
Papp Mihály <papp.misi@protonmail.com>
2019-08-27 13:58:21 +0200

    Alap API feltöltése

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:10.13-alpine
+ENV NODE_ENV production
+ENV PORT 80
+WORKDIR /usr/src/app
+COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"] 
+RUN npm install --production --silent && mv node_modules ../
+COPY . .
+EXPOSE 80
+CMD npm start
---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const redis = require("redis");
+const uuidv4 = require("uuid/v4");
+
+const PORT = process.env.PORT || 5000;
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+const client = redis.createClient(
+  REDIS_PORT,
+  process.env.NODE_ENV === "production" ? "redis" : "localhost"
+);
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  const longUrl = req.body;
+  const shortUrl = uuidv4();
+
+  client.set(shortUrl, longUrl, (err, reply) => {
+    if (reply === "OK") {
+      res.send(shortUrl);
+    }
+  });
+});
+
+app.get("/:shortUrl", (req, res) => {
+  const { shortUrl } = req.params;
+
+  client.get(shortUrl, (err, longUrl) => {
+    if (err) throw err;
+
+    if (longUrl) {
+      res.redirect(longUrl);
+    } else {
+      res.status(404).send("URL not found :(");
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}...`);
+});
